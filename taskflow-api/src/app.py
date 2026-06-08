1332a4cd132c05a71dfa80de04cbeb73297b99a2
ApoloLB <apololbastos@gmail.com>
2026-06-08 09:22:44 -0300

    chore: prepara aplicação para deploy

--- taskflow-api/src/app.py
+++ taskflow-api/src/app.py
@@ -49,3 +49,11 @@ def cadastro_livro():
 def home():
     return "Sistema de Gestão de Biblioteca"
 
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+     
+
