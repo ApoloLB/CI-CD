a3c592dfc72e208430aa2e171c8fe7b35e1c764e
ApoloLB <apololbastos@gmail.com>
2026-06-08 08:42:31 -0300

    Adiciona rota de listagem de livros

--- taskflow-api/src/app.py
+++ taskflow-api/src/app.py
@@ -22,3 +22,7 @@ def home():
 @app.route("/sobre")
 def sobre():
     return "Sistema desenvolvido em Flask para estudo de CI/CD"
+
+@app.route("/livros")
+def livros():
+    return "Lista de livros cadastrados"
